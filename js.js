1d606a1c494cb6c6b8e1fd5168f4ba91ef22b27b
Agustin Sanchez <agussanchez3687@gmail.com>
2023-03-07 21:07:47 -0300

    show hide en js

---
+++ js.js
@@ -0,0 +1,7 @@
+function ShowHide(){
+    if(document.getElementById("estudiosContent").style.display === "none"){
+        document.getElementById("estudiosContent").style.display = "block";
+    }else{
+        document.getElementById("estudiosContent").style.display = "none";
+    }
+}
